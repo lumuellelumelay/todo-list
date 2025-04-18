1ee5781a26987d6ab52a24f8fa1c6633f3255461
lumuellelumelay <168370509+lumuellelumelay@users.noreply.github.com>
2025-04-18 19:45:19 +0800

    Initial Todo Website

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './assets/styles/styles.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const testing = document.querySelector('.testing');
+  testing.textContent = 'Hello World!';
+});
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    watchFiles: ['./src/index.html'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+};
